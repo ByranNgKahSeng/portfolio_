7f4bfeed1d09c32b56ec9f8f9359b524b616506b
ByranNgKahSeng <TP068478@mail.apu.edu.my>
2023-12-10 18:22:01 +0800

    version 3.2

--- components/AchievementsSection.jsx
+++ components/AchievementsSection.jsx
@@ -19,18 +19,13 @@ const AchievementsSection = () => {
     return(
         <div className=" xl:gap-3 sm:py-3 xl:px-12">
             <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
-            {achievementsList.map((achievement) => {
-                return(
-                    <div 
-                        className="flex flex-col items-center justify-center mx-4">
-                        <h2 className="text-white text-4xl font-bold">
-                            {achievement.value}
-                        </h2>
-                        <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
-                    </div>
-                )
-            }
-            )}
+            {achievementsList.map((achievement, index) => (
+                //eslint-disable-next-line react/jsx-key
+                <div key={index} className="flex flex-col items-center justify-center mx-4">
+                    <h2 className="text-white text-4xl font-bold">{achievement.value}</h2>
+                    <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+                </div>
+            ))}
             </div>
             </div>
     )
